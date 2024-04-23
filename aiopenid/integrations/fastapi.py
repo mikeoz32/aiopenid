c39637401d2db4737e2577d450867518072b08e4
Mykhailo Kozlovskyi <myko@INTERLOGIC.COM.UA>
2024-04-23 13:54:04 +0300

    added security integration for FastAPI

--- aiopenid/integrations/fastapi.py
+++ aiopenid/integrations/fastapi.py
@@ -5,6 +5,7 @@ from aiopenid.oauth2 import BaseOauth2
 
 try:
     from fastapi import Request, Query
+    from fastapi.security import OAuth2AuthorizationCodeBearer as BaseCodeBearer
 except ImportError:
     raise Exception("FastAPI is not installed")
 
@@ -14,8 +15,17 @@ class OAuth2AuthorizationCodeCallback:
         self.client = client
 
     async def __call__(self, request: Request, code: Annotated[str, Query()]) -> Any:
-        redirect_url =f"{request.base_url}{request.url.path[1:]}" 
+        redirect_url = f"{request.base_url}{request.url.path[1:]}"
 
         return await self.client.authorization_code_callback(
             code=code, redirect_uri=redirect_url
+        )
+
+
+class OAuth2AuthorizationCodeBearer(BaseCodeBearer):
+    def __init__(self, client: BaseOauth2) -> None:
+        super().__init__(
+            authorizationUrl=client.authorize_endpoint,
+            tokenUrl=client.access_token_endpoint,
+            refreshUrl=client.refresh_token_endpoint,
         )
